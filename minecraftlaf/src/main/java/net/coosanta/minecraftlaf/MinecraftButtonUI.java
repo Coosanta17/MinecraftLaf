94545fb2b95a49cbac420da83a8917f0027f7337
Coosanta <coosantamapy@gmail.com>
2025-04-30 19:36:58 +1000

    Add MinecraftButtonUI class for custom button styling. Currently blank placeholder.

---
+++ minecraftlaf/src/main/java/net/coosanta/minecraftlaf/MinecraftButtonUI.java
@@ -0,0 +1,12 @@
+package net.coosanta.minecraftlaf;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicButtonUI;
+
+public class MinecraftButtonUI extends BasicButtonUI {
+
+    @Override
+    public void installUI(JComponent c) {
+        super.installUI(c);
+    }
+}
